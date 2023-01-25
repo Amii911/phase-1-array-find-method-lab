162c5e6ac2bae534fcf364de2a2cf64065b4f537
Aminat Adebayo <adebayo.aminat@gmail.com>
2023-01-25 10:25:50 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+
+
+function superbowlWin(array) {
+    const result = array.find(recordObj => recordObj.result === 'W')
+   if (result) {
+    return result.year
+   } 
+   return result
+}
+
